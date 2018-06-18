297348c916f8b342eab0408c56e989656a2d4211
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-06-18 12:16:40 +0200

    2.0.6

--- dist/external_d3_c3/keen-dataviz.js
+++ dist/external_d3_c3/keen-dataviz.js
@@ -1874,7 +1874,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.valueAtDeepKey = valueAtDeepKey;
 function valueAtDeepKey(obj, is, value) {
-  if (typeof is == 'string') return valueAtDeepKey(obj, is.split('.'), value);else if (is.length == 1 && value !== undefined) return obj[is[0]] = value;else if (is.length == 0) return obj;else if (typeof obj[is[0]] === 'undefined') return 'null';else return valueAtDeepKey(obj[is[0]], is.slice(1), value);
+  if (typeof is == 'string') return valueAtDeepKey(obj, is.split('.'), value);else if (is.length == 1 && value !== undefined) return obj[is[0]] = value;else if (is.length == 0) return obj;else if (typeof is === 'undefined' || typeof obj[is[0]] === 'undefined') return null;else return valueAtDeepKey(obj[is[0]], is.slice(1), value);
 }
 
 /***/ }),
